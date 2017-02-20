0a682161c9c849fdc4024028cc2a38f2092a2c7c
David <david.r.fankhauser@gmail.com>
2017-02-20 09:58:52 -0500

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action  :configure_permitted_parameters, if: :devise_controller?
+
   before_filter :categories, :brands
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_url, :alert => exception.message }
+    end
+  end
+
+  def line_items
+  	@line_items = LineItem.all
+  end
 
   def categories
   	@categories = Category.all
@@ -12,8 +24,12 @@ def brands
 	if @brands == nil 
 		@brands = Product.pluck(:brand).sort
 	end
-
 end
 
+protected 
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+	end
 end
 
---
+++ app/models/ability.rb
@@ -0,0 +1,24 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    if user.nil?
+
+        can :destroy, LineItem
+
+    elsif user.admin?
+
+        can :manage, Product
+        can :manage, Category
+        can :manage, Order
+        can :manage, LineItem
+
+    elsif user.guest?
+        can :destroy, LineItem
+    end
+end
+            
+            
+
+end
